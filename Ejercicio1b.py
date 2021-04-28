72d99f49231ff911380e575974cf82b54c59c5dd
unknown <arpineda@alumno.huergo.edu.ar>
2021-04-27 22:22:56 -0300

    Ejercicio 1 con subpunto b

---
+++ Ejercicio1b.py
@@ -0,0 +1,6 @@
+x1 = int(input("Ingrese el primer numero a multiplicar: "))
+x2 = int(input("Ingrese el segundo numero a multiplicar: "))
+
+producto = x1 * x2
+
+print("El resultado de la multiplicacion es: " + str(producto))
