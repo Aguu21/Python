21f6988b460d7d4431d13bca70f9f5ac27f7fb54
unknown <arpineda@alumno.huergo.edu.ar>
2021-04-27 22:31:15 -0300

    Ejercicio 2 con subpunto g

---
+++ Ejercicio2g.py
@@ -0,0 +1,6 @@
+import math
+
+def hipotenusaTriRect(catetoPrimero, catetoSegundo):
+    '''Calcula la hipotenusa de un triangulo rectangulo dados los catetos.'''
+    
+    return math.sqrt(catetoPrimero**2 + catetoSegundo**2)
