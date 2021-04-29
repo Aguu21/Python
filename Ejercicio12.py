7971aabd18972a8e09fb96b7c49d5d8dc7a905db
unknown <arpineda@alumno.huergo.edu.ar>
2021-04-28 22:30:14 -0300

    Ejercicio 12 arreglado, solo contaba la cantidad total de vocales antes

--- Ejercicio12.py
+++ Ejercicio12.py
@@ -1,16 +1,31 @@
 def cantidadVocales(cadenaUsuario):
-    '''Dada una cadena de caracteres devuelve la cantidad de vocales
-    en la misma'''
+    '''Dada una cadena de caracteres devuelve la cantidad de cada una de las
+    vocales en la misma'''
     
-    vocales = 0
     cadenaUsuario = cadenaUsuario.upper()
-    
-    for i in range (0, len(cadenaUsuario)):
-        if cadenaUsuario[i] in ["A", "E", "I", "O", "U"]:
-            vocales += 1
-    return vocales
+    cantidadAEIOU = [0, 0, 0, 0, 0]
 
+    for i in range (0, len(cadenaUsuario)):
+        
+        if cadenaUsuario[i] == "A":
+            cantidadAEIOU[0] += 1
+            
+        elif cadenaUsuario[i] == "E":
+            cantidadAEIOU[1] += 1
+            
+        elif cadenaUsuario[i] == "I":
+            cantidadAEIOU[2] += 1
+            
+        elif cadenaUsuario[i] == "O":
+            cantidadAEIOU[3] += 1
+            
+        elif cadenaUsuario[i] == "U":
+            cantidadAEIOU[4] += 1
+    return cantidadAEIOU
 
 cadenaUsuario = str(input("Ingrese una cadena de caracteres: "))
+vocales = cantidadVocales(cadenaUsuario)
 
-print ("La frase tiene : " + str(cantidadVocales(cadenaUsuario)) + " vocales.")
+print("La frase tiene " + str(vocales[0]) + " A's, " +
+      str(vocales[1]) + " E's, " + str(vocales[2]) + " I's, " +
+      str(vocales[3]) + " O's, " + str(vocales[4]) + " U's")
